8040a0e497f006cfdb6506f41cce2fcb3fb1afd3
tangxm <tangxm1314@foxmail.com>
2026-07-19 16:27:12 +0800

    Add IntelliJ IDEA 2025.4 cask

---
+++ Casks/intellij-idea@2025.4.rb
@@ -0,0 +1,50 @@
+cask "intellij-idea@2025.4" do
+  arch arm: "-aarch64"
+
+  version "2025.3.4,253.32098.37"
+  sha256 arm:   "3cec5a2d7e577e320810601eed0bf69a9628f3f1b82a33e40bf6472231bfc6b8",
+         intel: "7180c2ccf621d7f4bbed20b1f7172d41f050612bf6692d28ed539ebbeee58a64"
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.first}#{arch}.dmg"
+  name "IntelliJ IDEA Ultimate"
+  desc "Java IDE by JetBrains"
+  homepage "https://www.jetbrains.com/idea/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release"
+    strategy :json do |json|
+      json["IIU"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "intellij-idea@eap"
+
+  app "IntelliJ IDEA.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/idea.wrapper.sh"
+  binary shimscript, target: "idea"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      open -na "IntelliJ IDEA.app" --args "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Caches/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Logs/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.intellij.plist",
+    "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+    "~/Library/Preferences/jetbrains.idea.*.plist",
+    "~/Library/Saved Application State/com.jetbrains.intellij.savedState",
+  ]
+end
