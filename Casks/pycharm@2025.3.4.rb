209ec041ce41755840c26bb4047b2c11c8622f03
tangxm <tangxm1314@foxmail.com>
2026-07-19 19:30:32 +0800

    Add Pycharm 2025.3.4

---
+++ Casks/pycharm@2025.3.4.rb
@@ -0,0 +1,53 @@
+cask "pycharm@2025.3.4" do
+  arch arm: "-aarch64"
+
+  version "2025.3.4,253.32098.74"
+  sha256 arm:   "ec7053cec8b9746feae80a8c28f07317220077143db53c152082baa695ad88c6",
+         intel: "6d1f3b919ac25610e2f646e2f5837992540552fa51a3499fbdff44eaadabf15e"
+
+  url "https://download.jetbrains.com/python/pycharm-professional-#{version.csv.first}#{arch}.dmg"
+  name "PyCharm"
+  name "PyCharm Professional"
+  desc "IDE for professional Python development"
+  homepage "https://www.jetbrains.com/pycharm/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release"
+    strategy :json do |json|
+      json["PCP"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+
+  app "PyCharm.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/pycharm.wrapper.sh"
+  binary shimscript, target: "pycharm"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/PyCharm.app/Contents/MacOS/pycharm' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/PyCharm#{version.major_minor}",
+    "~/Library/Application Support/PyCharm#{version.major_minor}",
+    "~/Library/Caches/JetBrains/PyCharm#{version.major_minor}",
+    "~/Library/Logs/JetBrains/PyCharm#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.pycharm.plist",
+    "~/Library/Preferences/jetbrains.pc.*.plist",
+    "~/Library/Preferences/jetbrains.py.*.plist",
+    "~/Library/Preferences/jetbrains.pycharm.*.plist",
+    "~/Library/Preferences/PyCharm#{version.major_minor}",
+    "~/Library/Saved Application State/com.jetbrains.pycharm.savedState",
+  ]
+end
