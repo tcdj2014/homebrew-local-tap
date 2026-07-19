8fb2b4eb53bf28256d0f8aba4b664e195b3ce70e
tangxm <tangxm1314@foxmail.com>
2026-07-19 16:39:36 +0800

    Add Android Studio 2025.3.2.6 cask

---
+++ Casks/android-studio@2025.3.2.6.rb
@@ -0,0 +1,43 @@
+cask "android-studio" do
+  arch arm: "mac_arm", intel: "mac"
+
+  version "2025.3.2.6,panda2"
+  sha256 arm:   "6c9944ce8dac9b5dd05e96a46f2e042628ced568557d29d1299adf5dad817cb7",
+         intel: "e2ba41245f20ce3d38e9699ca5acb68fe965d06cc0b69188764c5dcbbe97455d"
+
+  url "https://edgedl.me.gvt1.com/android/studio/install/#{version.csv.first}/android-studio#{"-#{version.csv.second}" if version.csv.second}-#{arch}.dmg",
+      verified: "edgedl.me.gvt1.com/android/studio/install/"
+  name "Android Studio"
+  desc "Tools for building Android applications"
+  homepage "https://developer.android.com/studio/"
+
+  livecheck do
+    url :homepage
+    regex(%r{href=.*?/v?(\d+(?:\.\d+)+)/android[._-]studio(?:[._-]([^"' >]+))?[._-]#{arch}\.dmg}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        match[1].present? ? "#{match[0]},#{match[1]}" : match[0]
+      end
+    end
+  end
+
+  auto_updates true
+
+  app "Android Studio.app"
+  binary "#{appdir}/Android Studio.app/Contents/MacOS/studio"
+
+  zap trash: [
+        "~/.android",
+        "~/Library/Android/sdk",
+        "~/Library/Application Support/Google/AndroidStudio#{version.major_minor}",
+        "~/Library/Caches/Google/AndroidStudio#{version.major_minor}",
+        "~/Library/Logs/Google/AndroidStudio#{version.major_minor}",
+        "~/Library/Preferences/com.android.Emulator.plist",
+        "~/Library/Preferences/com.google.android.studio.plist",
+        "~/Library/Saved Application State/com.google.android.studio.savedState",
+      ],
+      rmdir: [
+        "~/AndroidStudioProjects",
+        "~/Library/Android",
+      ]
+end
