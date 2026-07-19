6c9acd5df6808d22354eba2031160750188341ce
tangxm <tangxm1314@foxmail.com>
2026-07-19 22:38:12 +0800

    Add Android Studio 2025.1.1.14 cask

---
+++ Casks/android-studio.rb
@@ -0,0 +1,39 @@
+cask "android-studio" do
+  arch arm: "mac_arm", intel: "mac"
+
+  version "2025.1.1.14"
+  sha256 arm:   "93e2320e619829a9d0970efbac0315174ef09ec5d19adde99b531bde0648548a",
+         intel: "9421148bf1421ee3d94b8faa1fe270ecde15c0eb6a36f147eccab5a01d9ba8aa"
+
+  url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
+      verified: "redirector.gvt1.com/edgedl/android/studio/"
+  name "Android Studio"
+  desc "Tools for building Android applications"
+  homepage "https://developer.android.com/studio/"
+
+  livecheck do
+    url :homepage
+    regex(/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Android Studio.app"
+  binary "#{appdir}/Android Studio.app/Contents/MacOS/studio"
+
+  zap trash: [
+        "~/.android",
+        "~/Library/Android/sdk",
+        "~/Library/Application Support/Google/AndroidStudio#{version.major_minor}",
+        "~/Library/Caches/Google/AndroidStudio#{version.major_minor}",
+        "~/Library/Logs/Google/AndroidStudio#{version.major_minor}",
+        "~/Library/Preferences/com.android.Emulator.plist",
+        "~/Library/Preferences/com.google.android.studio.plist",
+        "~/Library/Saved Application State/com.google.android.studio.savedState",
+      ],
+      rmdir: [
+        "~/AndroidStudioProjects",
+        "~/Library/Android",
+      ]
+end
