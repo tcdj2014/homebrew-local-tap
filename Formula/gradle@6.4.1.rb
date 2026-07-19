0fb81a512dccc28c42e9096359158d1b33c46e44
tangxm <tangxm1314@foxmail.com>
2026-07-19 17:26:34 +0800

    Add Gradle 6.9 6.4.1

--- Formula/gradle@6.4.1.rb
+++ Formula/gradle@6.4.1.rb
@@ -4,20 +4,16 @@ class GradleAT641 < Formula
   url "https://services.gradle.org/distributions/gradle-6.4.1-all.zip"
   sha256 "3fd824892df8ad5847be6e4fb7d3600068437de172939fd657cc280a1a629f63"
 
-  bottle :unneeded
-
   depends_on "openjdk"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin docs lib src]
-    (bin/"gradle").write_env_script libexec/"bin/gradle",
-      :JAVA_HOME => "${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+    (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/gradle --version")
-
     (testpath/"build.gradle").write <<~EOS
       println "gradle works!"
     EOS
