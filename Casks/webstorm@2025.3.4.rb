f2f3e5146ca1006cf02918853ccfb073b00e5f91
tangxm <tangxm1314@foxmail.com>
2026-07-19 19:26:49 +0800

    Add Webstorm 2025.3.4

---
+++ Casks/webstorm@2025.3.4.rb
@@ -0,0 +1,51 @@
+cask "webstorm@2025.3.4" do
+  arch arm: "-aarch64"
+
+  version "2025.3.4,253.32098.39"
+  sha256 arm:   "c5a89abdfbdb00e9dddd342cdce419c2cf4d593012b8eeea9fe720430f16f27b",
+         intel: "5c24ff1f504b3f682226aaeab465d970f9367fe16f52d271d8749c22eca7da45"
+
+  url "https://download.jetbrains.com/webstorm/WebStorm-#{version.csv.first}#{arch}.dmg"
+  name "WebStorm"
+  desc "JavaScript IDE"
+  homepage "https://www.jetbrains.com/webstorm/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=WS&latest=true&type=release"
+    strategy :json do |json|
+      json["WS"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates true
+
+  app "WebStorm.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/webstorm.wrapper.sh"
+  binary shimscript, target: "webstorm"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/WebStorm.app/Contents/MacOS/webstorm' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/WebStorm#{version.major_minor}",
+    "~/Library/Caches/com.apple.nsurlsessiond/Downloads/com.jetbrains.WebStorm",
+    "~/Library/Caches/JetBrains/WebStorm#{version.major_minor}",
+    "~/Library/Logs/JetBrains/WebStorm#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.WebStorm.plist",
+    "~/Library/Preferences/jetbrains.webstorm.*.plist",
+    "~/Library/Preferences/WebStorm#{version.major_minor}",
+    "~/Library/Preferences/webstorm.plist",
+    "~/Library/Saved Application State/com.jetbrains.WebStorm.savedState",
+  ]
+end
